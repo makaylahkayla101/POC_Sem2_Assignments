26c2a4609e4d04d37211cdac620e531905c43839
makaylah walton <waltomak000@warren.k12.in.us>
2024-02-01 10:05:58 -0500

    l1

---
+++ unit 5/lesson2/assigment.py
@@ -0,0 +1,18 @@
+number1 = 5
+number2 = 10
+try:
+    number1 = int(input("5"))
+   
+except ValueError:
+    
+    pass 
+else
+    print("No value error detected")
+finally:
+    print("Values taken care of")
+
+try:5/10
+  
+    pass 
+except ZeroDivisionError: incorrect
+  pass  
