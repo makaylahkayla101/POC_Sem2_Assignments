68cd159729140f89a1f9bf271178ae0c3a0abf87
makaylah walton <waltomak000@warren.k12.in.us>
2024-02-08 10:02:43 -0500

    u6 l3

---
+++ unit6/lesson 3/assigment.py
@@ -0,0 +1,7 @@
+import platform
+import random
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+names = ['jessica', 'cory', 'maya', 'hailey', 'paige', 'naomi']
+names = ['jessica', 'cory', 'Maya', 'hailey', 'paige', 'naomi']
+
+print(random.sample(names, 3))
