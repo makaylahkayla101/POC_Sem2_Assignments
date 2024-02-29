1bc8d07319b6fd40dc1f20db0ffbe4092a811c2b
makaylah walton <waltomak000@warren.k12.in.us>
2024-02-29 09:46:45 -0500

    u8 l1

---
+++ unit8/lesson1/assignmen.py
@@ -0,0 +1,11 @@
+class RightTriangle: 
+    def __init__(self, base, height):
+        self.base = base
+      
+        pass
+    
+    def area(self):1/2 base
+       
+        pass 
+triangle_1 = RightTriangle(2,8)
+print("The area of triange_1 is 2,8", triangle_1.area(2,8))
