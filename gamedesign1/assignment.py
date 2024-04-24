ec162fbcd6c3e8c348c93d4bdc72776c1e930816
makaylah walton <waltomak000@warren.k12.in.us>
2024-04-24 09:33:15 -0400

    l8 u2

---
+++ gamedesign1/assignment.py
@@ -0,0 +1,26 @@
+grid+[
+   [" "," "," "]
+  [" "," "," ",]
+   [" "," "," "]
+]
+def print_grid():
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            print(grid[row][col], end="")
+            if(col !=2):
+                print("*****")
+
+
+
+def is_bad_choice(choice : str)
+
+def game_loop()
+    print("welcome to TIC TAC TOE")
+    while(true):
+        user_request = input("enter STOP to end or anything else to contnue")
+        if user_request._eq_("STOP"):
+        print("loop is running")
+        user_choice = ""
+        print ("GAME OVER")
+
+game_loop()
